3a4132d7236f5512933d755fa262f2d48f6714fe
Albert Miftar <a.miftar@aa.mk>
2020-06-24 15:44:52 +0200

    Updates

---
+++ precache-manifest.28cf23d89efd57773b9d72f06e8399e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f209fefc90084769424ce981eff66b",
+    "url": "/posts-reactapp/index.html"
+  },
+  {
+    "revision": "a98a5cc453b5b428cad7",
+    "url": "/posts-reactapp/static/css/main.6ad8d2a5.chunk.css"
+  },
+  {
+    "revision": "fa9a25cdd8f5cf15c704",
+    "url": "/posts-reactapp/static/js/2.746ccb4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/posts-reactapp/static/js/2.746ccb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98a5cc453b5b428cad7",
+    "url": "/posts-reactapp/static/js/main.6afef14b.chunk.js"
+  },
+  {
+    "revision": "3eb0b872e451920e5074",
+    "url": "/posts-reactapp/static/js/runtime-main.482a553f.js"
+  }
+]);
