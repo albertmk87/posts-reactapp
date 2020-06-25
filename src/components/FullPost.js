0c294f101a2e068aa061378a907d36473c9012ab
Albert Miftar <a.miftar@aa.mk>
2020-06-25 10:11:58 +0200

    commit

--- src/components/FullPost.js
+++ src/components/FullPost.js
@@ -10,31 +10,39 @@ class FullPost extends React.Component{
 
     }
 
-   componentDidUpdate () {
-        if ( this.props.currentPost) {
-            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id!==this.props.currentPost)){
-                axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.currentPost )
-                    .then( response => {
-                        // console.log(response);
-                        this.setState( { loadedPost: response.data } );
-                    } );
-            }
+   // componentDidUpdate () {
+   //      if ( this.props.currentPost) {
+   //          if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id!==this.props.currentPost)){
+   //              axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.currentPost )
+   //                  .then( response => {
+   //                      // console.log(response);
+   //                      this.setState( { loadedPost: response.data } );
+   //                  } );
+   //          }
    
-        }
-    }
+   //      }
+   //  }
 
 
 	render(){
+		let passedPost='';
+		if(this.props.currentPost){
+		passedPost=this.props.posts.find(post=>post.id===this.props.currentPost);
+			
+
+
+		}
+	
 	
 	let cur='';
 	let result=<p>Please select a post</p>
 	if(this.props.currentPost){
-		if(this.state.loadedPost){
-			console.log(this.state.loadedPost)
+		if(passedPost){
+			console.log(passedPost)
 		result=(
 				<Auxiliary>
-				<h2>{this.state.loadedPost.title} </h2>
-				<p>{this.state.loadedPost.body}</p>
+				<h2>{passedPost.title} </h2>
+				<p>{passedPost.body}</p>
 				<button onClick={this.props.deletePost}> Delete </button>
 				</Auxiliary>
 			)
